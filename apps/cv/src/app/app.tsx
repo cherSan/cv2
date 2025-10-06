3b189fa27bb2d973f9ee4cea0b9d8931a173fd28
chersan <chernushevich.alexander@gmail.com>
2025-10-06 12:57:37 +0700

    fix mobile

--- apps/cv/src/app/app.tsx
+++ apps/cv/src/app/app.tsx
@@ -1,6 +1,18 @@
 import { TerminalComponent } from './terminal/terminal.component';
+import { useMemo } from 'react';
 
 export const Application = () => {
+  const isMobile = useMemo(() => /Mobi|Android|iPhone/i.test(navigator.userAgent), []);
+
+  if (isMobile) {
+    return (
+      <div className="mobile-warning">
+        <p>
+          This application is not optimized for mobile devices. Please use a desktop or laptop computer.
+        </p>
+      </div>
+    )
+  }
   return (
     <TerminalComponent />
   );
